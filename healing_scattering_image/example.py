cdef346908763ea34602abedf66fb2eef526c796
Jiliang Liu <jiliangliu@bnl.gov>
2018-07-25 18:54:43 -0400

    re-edit code piece by piece

---
+++ healing_scattering_image/example.py
@@ -0,0 +1,147 @@
+import numpy as np
+from skimage.draw import polygon
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from scipy import ndimage
+plt.close('all')
+
+sample = '/Users/jiliangliu/Desktop/CMS_for_ML/2011_04Apr_30-Fang_shapedNPs/mini_image/lin_bs-CB_ring_top_20sec_frame9_SAXS.mat'
+im = loadmat(sample)['detector_image'].astype(float)
+
+im[175:180,:] = np.nan
+im[230:235,:] = np.nan
+im[93:223,124:132] = np.nan
+r = [218,218,185,190,222,222]
+c = [130,145,242,242,145,130]
+
+rr,cc = polygon(r,c)
+mask = np.zeros(np.shape(im)).astype(bool)
+mask[rr,cc] = True
+mask[175:180,:] = True
+mask[230:235,:] = True
+mask[93:223,124:132] = True
+
+im[mask] = np.nan
+
+fig,ax = plt.subplots()
+plt.imshow(np.log(im))
+
+beam_x = 129
+beam_y = 194
+
+im[im <= 0.] = 1e-6
+
+from healing_scattering_image_packaging import polar_coord,polar_coord_float,qphi_image,oneD_intensity
+
+qphi_image_1 = qphi_image(im,beam_x,beam_y,4,220,720)
+
+fig,ax = plt.subplots()
+plt.imshow(np.log(qphi_image_1))
+
+xcenter = beam_x
+ycenter = beam_y
+r_min = 4
+r_max = 220
+angle_resolution = 720
+
+
+qphi_image_1[np.isnan(qphi_image_1)] = 1e-6
+qphi_image_1[qphi_image_1 < 1e-6] = 1e-6
+
+qphi_transform_mask = qphi_image(np.ones(np.shape(im)),xcenter,ycenter,r_min,r_max,angle_resolution)
+#keep the transform mask
+qphi_image_1[np.isnan(qphi_transform_mask)] = np.nan
+#the mask correlates to the 
+qphi_image_mask = (qphi_image_1 == 1e-6)
+
+#cut qphi_image_1 to r_max portion and make intensity to log scale which will make intensity comparable in variation scale
+#rotate to avoid mask at edge of qphi_image because mask at horizon or vertical to center
+qphi_image_2 = np.roll(qphi_image_1[:,:r_max],90,axis=0)
+qphi_image_2[qphi_image_2 < -6] = -6
+qphi_image_2_mask = (qphi_image_2 == -6)
+qphi_image_2_maks = ndimage.binary_dilation(qphi_image_2_mask,np.ones((2,2)))
+#dilute the mask ensure correct cover the edge of mask
+qphi_image_2[qphi_image_2_mask] = -6
+#feel like qphi_image_3 is redundant
+qphi_image_3 = np.copy(qphi_image_2)
+qphi_image_3[qphi_image_3 == -6] = np.nan
+radius,azimuth = polar_coord(im,xcenter,ycenter,angle_resolution)
+r_f,a_f = polar_coord_float(im,xcenter,ycenter,angle_resolution)
+
+#im[im<1] = 1
+I = oneD_intensity(im=im,xcenter=xcenter,ycenter=ycenter,mask=mask).cir_ave()
+fig,ax = plt.subplots()
+ax.plot(I)
+
+I = I[:r_max]
+iso_judge     = np.zeros((len(I),))
+
+iso_std                   = np.zeros((len(I),))
+iso_local_std             = np.zeros((len(I),))
+iso_judge_global          = np.zeros((len(I),))
+iso_judge_local_vs_global = np.zeros((len(I),))
+
+delta_width = 1
+
+qphi_image_1[qphi_image_1==1e-6] = np.nan
+
+for _ in range(r_max):
+    if (np.size(qphi_image_3[np.isnan(qphi_image_3[:,_])==0,_]) >
+       angle_resolution/10):
+       iso_judge_global[_] = np.std(qphi_image_3[
+                             np.isnan(qphi_image_3[:,_]) == 0,_])
+    else:
+       iso_judge_global[_] = np.nan
+    
+    # here try to calculate the intenity in each q with smooth of delta_width of q
+    if   (_ > delta_width) and (_ < len(I)-delta_width):
+       Ave_I = np.sum(qphi_image_3[:, (_-delta_width) : (_+delta_width)],
+                      axis = 1) / (2*delta_width)
+    elif (_ < delta_width):
+       Ave_I = np.sum(qphi_image_3[:, _ : (_+delta_width)],
+                      axis = 1) / delta_width
+    elif (_ > len(I) - delta_width):
+       Ave_I = np.sum(qphi_image_3[:, (_-delta_width) : _],
+                      axis = 1) / delta_width
+    
+    Ave_I[ Ave_I < np.nanmean(Ave_I) - 0.7*np.abs(np.nanmean(Ave_I))] = np.nan
+    Ave_I_mean = np.concatenate((Ave_I[np.isnan(Ave_I)==0],
+                                 Ave_I[np.isnan(Ave_I)==0],
+                                 Ave_I[np.isnan(Ave_I)==0]))
+    Ave_I_local_std = np.zeros((len(Ave_I[np.isnan(Ave_I)==0]),))
+    len_nonnan_Ave_I = len(Ave_I[np.isnan(Ave_I)==0])
+    for __ in range(len_nonnan_Ave_I,2*len_nonnan_Ave_I):
+        Ave_I_local_std[__-len_nonnan_Ave_I] = np.std(
+                                               Ave_I_mean[(__-int(angle_resolution/100)):
+                                               (__+int(angle_resolution/100))])
+        
+    iso_local_std[_] = np.abs(np.nanmean(Ave_I_local_std))
+    iso_std[_]  = np.std(Ave_I_mean[len_nonnan_Ave_I:2*len_nonnan_Ave_I])
+    iso_judge_local_vs_global[_] = iso_local_std[_] / iso_std[_]
+    
+x,y = np.histogram(iso_judge_global[np.isnan(iso_judge_global)==0] ,bins=30)
+#make x,y elements corresponisible
+y = y[:-1] + np.diff(y)/2
+
+from scipy.misc import factorial
+
+def poisson_func(k,Lambda):
+    return ((Lambda*1.)**k/factorial(k))*np.exp(-Lambda)
+
+from scipy.optimize import curve_fit
+
+k = np.arange(1,31,1)
+
+if (np.size(x[np.isnan(x)]) > 0) or (np.size(x[x==0]) == len(x)):
+   #return np.zeors(np.shape(im))
+   print('break')
+lamb = curve_fit(poisson_func,k,x*1./np.nansum(x),bounds = [0,np.inf])[0][0]
+
+x1,y1 = np.histogram(iso_judge_local_vs_global[
+                    (np.abs(iso_judge_local_vs_global) != np.inf) &
+                    (np.isnan(iso_judge_local_vs_global) == 0)], 
+                    bins = 30)
+y1 = y1[1:] + np.diff(y1)/2.
+lamb1 = curve_fit(poisson_func,k,x1*1./np.nansum(x1),bounds = [np.argmax(x1)-1,np.argmax(x1)+1])[0][0]
+
+plt.show()
