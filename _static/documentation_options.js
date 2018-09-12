87d2506605b1bbe47ae0ee036db9b4b533a6c86e
Travis CI User <travis@example.org>
2018-09-12 15:21:12 +0000

    Update docs after building Travis build 15 of ligerliu/healing_scattering_image
    The docs were built from the branch 'master' against the commit
    23126b8648120ee3fa268d14b350cb897d0e44a3.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ligerliu/healing_scattering_image/jobs/427736002.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post25+g1380294',
+    VERSION: '0.post26+g23126b8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
